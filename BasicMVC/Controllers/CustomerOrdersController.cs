b6f613dde57cb42c3d32cbb8b7b18676bf8af46d
GÖZ'SS <GZ'SS@DESKTOP-NKTDPLS>
2024-09-06 00:17:45 +0300

    Add project files.

---
+++ BasicMVC/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,50 @@
+﻿using BasicMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicMVC.Controllers
+{
+    public class CustomerOrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            Customer customer = new Customer()
+            {
+                Id= 1,
+                FirstName="Ahmet",
+                LastName="Göz",
+                Email = "agoz@gmail.com"
+            };
+
+            Order order = new Order()
+            {
+                Id = 1,
+                ProductName ="Mobile Phone",
+                Price = 24480,
+                Quantity = 1,
+
+            };
+            Order order1 = new Order()
+            {
+                Id = 2,
+                ProductName = "Laptop",
+                Price = 35000,
+                Quantity = 1,
+
+            };
+            List<Order> orders = new List<Order>();
+            orders.Add(order);
+            orders.Add(order1);
+
+            var viewModel = new CustomerOrderViewModel()
+            {
+                Customer = customer,
+                Orders = orders
+                
+
+            };
+
+
+            return View(viewModel);
+        }
+    }
+}
